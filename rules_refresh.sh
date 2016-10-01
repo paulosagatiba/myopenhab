e62038edc339ed711792e42a13a6ec12d942d6a1
Kail Keusch <kfkkail@gmail.com>
2016-10-01 14:10:03 -0400

    Wrong location of rules folder

--- rules_refresh.sh
+++ rules_refresh.sh
@@ -1,6 +1,6 @@
 # This script is called by openHAB after persistance service was started
 sleep 5
-cd /opt/openhab/configurations/rules/
+cd /etc/openhab/configurations/rules/
 RULES=`find *.rules | grep -v refresh.rules`
 for f in $RULES
 do
