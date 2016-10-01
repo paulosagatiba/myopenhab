75eee521657f35099f7b7d68e948819dd566f76e
Kail Keusch <kfkkail@gmail.com>
2016-10-01 16:16:56 -0400

    Removing the refresh rule
    Causing issues with startup after I changed the directory

--- rules_refresh.sh
+++
@@ -1,8 +0,0 @@
-# This script is called by openHAB after persistance service was started
-sleep 5
-cd /etc/openhab/configurations/rules/
-RULES=`find *.rules | grep -v refresh.rules`
-for f in $RULES
-do
-  touch $f
-done
